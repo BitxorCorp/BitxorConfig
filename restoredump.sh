06ef9456c4c4575dcf85f656a4524a46f353f8ea
Kelvin Romero <admin@sulachain.com>
2023-07-13 11:18:29 -0700

    fix

--- restoredump.sh
+++ restoredump.sh
@@ -8,7 +8,10 @@ mv /root/BitxorCore/data/harvesters.dat harvesters.dat
 rm -rf /root/BitxorCore/data
 tar -xzf Data.tar.gz -C /root/BitxorCore/
 mv harvesters.dat /root/BitxorCore/data/harvesters.dat
-mongo --eval 'use bitxorcore; db.dropDatabase();'
+echo 'use bitxorcore' > commands.js
+echo 'db.dropDatabase()' >> commands.js
+mongo < commands.js
+rm commands.js
 mkdir dbdump
 tar -xzf BitxorCoreDBdump.tar.gz -C dbdump
 mongorestore --db=bitxorcore dbdump/bitxorcore
