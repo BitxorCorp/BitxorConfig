6ba503c0ca4af03fc5ce25c14f1bfdeaa8323845
Kelvin Romero <admin@sulachain.com>
2023-07-15 07:50:45 -0700

    restorefix

--- restoredump.sh
+++ restoredump.sh
@@ -8,6 +8,20 @@ mv /root/BitxorCore/data/harvesters.dat harvesters.dat
 rm -rf /root/BitxorCore/data
 tar -xzf Data.tar.gz -C /root/BitxorCore/
 mv harvesters.dat /root/BitxorCore/data/harvesters.dat
+sed -i.bak 's/\(fileDatabaseBatchSize = \).*/\1100000/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(blockDisruptorSlotCount = \).*/\18192/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(blockElementTraceInterval = \).*/\110/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(blockDisruptorMaxMemorySize = \).*/\1600MB/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(transactionDisruptorSlotCount = \).*/\18192/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(transactionElementTraceInterval = \).*/\110/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(transactionDisruptorMaxMemorySize = \).*/\1200MB/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(enableStatistics = \).*/\1false/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(maxOpenFiles = \).*/\10/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(maxBackgroundThreads = \).*/\10/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(maxSubcompactionThreads = \).*/\10/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(blockCacheSize = \).*/\10MB/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(memtableMemoryBudget = \).*/\10MB/' /root/BitxorCore/resources/config-node.properties
+sed -i.bak 's/\(maxWriteBatchSize = \).*/\1500MB/' /root/BitxorCore/resources/config-node.properties
 echo 'use bitxorcore' > commands.js
 echo 'db.dropDatabase()' >> commands.js
 mongo < commands.js
